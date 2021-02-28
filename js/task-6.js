75588b4c482cffcc637422af97b77bb0a83e3c57
Tetiana Lobko <alsedexl@gmail.com>
2021-03-01 00:06:02 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,7 +3,8 @@ const inputRef = document.querySelector('#validation-input');
 const addClass = (event) => {
     (Number(inputRef.dataset.length) === event.target.value.length)
         ? (inputRef.classList.add('valid'), inputRef.classList.remove('invalid'))
-        : inputRef.classList.add('invalid');
+        : (inputRef.classList.add('invalid'), inputRef.classList.remove('valid'));
+    if (event.target.value === '') inputRef.classList.remove('invalid');
 };
 
 inputRef.addEventListener('blur', addClass);
